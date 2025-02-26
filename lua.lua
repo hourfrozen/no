6006e735f04e41e5dab71befe048f3f3593cf2b3
hourfrozen <hourfrozen4@gmail.com>
2025-02-26 14:35:48 -0500

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -4,7 +4,7 @@ d = game.Workspace:GetDescendants()
 
 for i,v in pairs(d) do
     if v:IsA("BasePart") then
-        v.Color = Color.New(math.random(-255,255),math.random(-255,255),math.random(-255,255))
+        v.Color = Color3.New(math.random(-255,255),math.random(-255,255),math.random(-255,255))
     end
 end
 end
